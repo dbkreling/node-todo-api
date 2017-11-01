655123bb345e0b73e705482af48519d0c20a4e47
Daniel Kreling <dbkreling@gmail.com>
2017-11-01 06:53:38 -0200

    server: add POST /users route
    This commit introduces a POST route for users at /users. The model is added a
    validator, that verifies if email and a new added password fields are valid.
    
    Signed-off-by: Daniel Kreling <dbkreling@gmail.com>

--- server/server.js
+++ server/server.js
@@ -14,6 +14,7 @@ const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
+// create a new todo
 app.post('/todos', (req, res) => {
     var todo = new Todo({
         text: req.body.text
@@ -100,6 +101,18 @@ app.patch('/todos/:id', (req, res) => {
     }).catch((e) => {
         return res.status(400).send();
     });
+});
+
+// create a new user
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    var user = new User(body);
+
+    user.save().then((user) => {
+        res.send(user);
+    }).catch((e) => {
+        res.status(400).send(e);
+    })
 });
 
 app.listen(port, () => {
