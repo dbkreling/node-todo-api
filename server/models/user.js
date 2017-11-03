45a24c56134d4c7512a4fdce6aa7f1a44ce69cac
Daniel Kreling <dbkreling@gmail.com>
2017-11-03 07:04:02 -0200

    server: add token method for POST /users
    This commit introduces an authentication method for users, making use of UserSchema
    on the user model to create methods inside the model. The method generates a token and
    adds it to the header of the REST response.
    
    It also trims off some sensitive information from the body, by implementing
    a toJSON() method inside the user model.
    
    Ref.: Lecture 90
    
    Signed-off-by: Daniel Kreling <dbkreling@gmail.com>

--- server/models/user.js
+++ server/models/user.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash')
 
-var User = mongoose.model('User', {
+var UserSchema = new mongoose.Schema({
     email: {
         type: String,
         minlength: 1,
@@ -29,5 +31,30 @@ var User = mongoose.model('User', {
         }
     }]
 });
+
+UserSchema.methods.toJSON = function () {
+    var user = this;
+    var userObject = user.toObject();
+
+    return _.pick(userObject, ['_id', 'email']);
+};
+
+// function will be used, because 'function' has access to 'this'
+UserSchema.methods.generateAuthToken = function () {
+    var user = this;
+    var access = 'auth';
+    // sintax of sign(object_to_be_signed, 'secret-string').toString()
+    var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+
+    // 'tokens' is a regular array, so use array methods to update it
+    user.tokens.push({access, token});
+
+    // refer to Lecture 90 (8:46) in order to understand this return chain
+    return user.save().then(() => {
+        return token; // 'token' will be passed as the success argument for the next 'then' call (on server.js)
+    });
+};
+
+var User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
--- server/server.js
+++ server/server.js
@@ -103,13 +103,15 @@ app.patch('/todos/:id', (req, res) => {
     });
 });
 
-// create a new user
+// POST /users (create a new user)
 app.post('/users', (req, res) => {
     var body = _.pick(req.body, ['email', 'password']);
     var user = new User(body);
 
-    user.save().then((user) => {
-        res.send(user);
+    user.save().then(() => {
+        return user.generateAuthToken(); // we're expecting a chaining promise. Call 'then'
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
     }).catch((e) => {
         res.status(400).send(e);
     })
